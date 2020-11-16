83da49162ee55ce0bceca877e436d75834c88a02
Asim Bera <asimbera@outlook.in>
2020-11-16 19:57:57 +0530

    fix logic

--- programs/A02_P06.c
+++ programs/A02_P06.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-// FIXME logic
 int main()
 {
   int a;
   printf("Enter a year (4 digit) => ");
   scanf("%d", &a);
 
-  if (a >= 0 && (a % 400 == 0 || a % 4 == 0))
+  if (a % 100 == 0)
+  {
+    if (a % 400 == 0)
+    {
+      printf("%d is a century and a leap year.\n", a);
+    }
+    else
+    {
+      printf("%d is not a leap year or invalid.\n", a);
+    }
+  }
+  else if (a % 4 == 0)
   {
     printf("%d is a leap year.\n", a);
   }
